f4e529f9927422bcb617e8ed1892d8eb2a6ec32e
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-11-13 11:49:16 -0600

    Added full screen map to app

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCzwr1K00UKJjM93xsYA6A9xkosywtWKtI&callback=initMap";
+    script.async = true;
+    script.defer = true;
+
+    document.body.appendChild(script);
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
 }
