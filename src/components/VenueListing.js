443c2ec48fde666c78adb619a1b85b4182237ab5
Megan Spaulding <meganspauldingcreative@gmail.com>
2018-11-13 23:08:20 -0600

    Added styling to sidebar

--- src/components/VenueListing.js
+++ src/components/VenueListing.js
@@ -6,7 +6,7 @@ class VenueListing extends Component {
 	render() {
 		console.log(this.props.venue)
 		return(
-			<div>
+			<div className="venue-listing">
 				<h3 className="venue-name">{this.props.venue.venue.name}</h3>
 				<h5 className="venue-category">{this.props.venue.venue.categories[0].name || "n/a"}</h5>
 				<p className="venue-address"> {this.props.venue.venue.location.address || "Jackson, TN"} </p>
